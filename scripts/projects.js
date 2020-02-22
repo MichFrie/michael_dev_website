0ce689c01bdb6356cfdb062820a228033ea0809e
Michael F <mifrpost@gmail.com>
2020-02-23 00:37:42 +0100

    new project design

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,23 +1,12 @@
-let codePreview_1 = document.querySelector(".code_preview_1");
-let codePreview_2 = document.querySelector(".code_preview_2");
-let codePreview_3 = document.querySelector(".code_preview_3");
-let codePreview_4 = document.querySelector(".code_preview_4");
-let codePreview_5 = document.querySelector(".code_preview_5");
-
+let preview1 = document.querySelector(".code_preview_1");
 let buttonPreview = document.querySelector(".button_preview");
 
-//Preview window initial state
-codePreview_1.style.visibility = "hidden";
+preview1.style.visibility = "hidden";
 
-
-
-//Visibility preview 1
 buttonPreview.addEventListener('click', function(){
-    if(codePreview_1.style.visibility == "visible"){
-        codePreview_1.style.visibility = "hidden";
+    if(preview1.style.visibility == "hidden"){
+        preview1.style.visibility = "visible";
     }else{
-        codePreview_1.style.visibility = "visible";
+        preview1.style.visibility = "hidden";
     }
 })
-
-//Visibility preview 2

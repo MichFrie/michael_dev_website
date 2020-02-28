29adbdaf0c75e7ae098c3f005b8920eb2d5dc2bb
Michael F <mifrpost@gmail.com>
2020-02-28 23:43:20 +0100

    removed background from gists

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,12 +1,23 @@
 let preview1 = document.querySelector(".code_preview_1");
-let buttonPreview = document.querySelector(".button_preview");
+let preview2 = document.querySelector(".code_preview_2");
+let buttonPreview1 = document.querySelector(".button_preview_1");
+let buttonPreview2 = document.querySelector(".button_preview_2");
 
-preview1.style.visibility = "hidden";
+preview1.style.display = "none";
+preview2.style.display = "none  ";
 
-buttonPreview.addEventListener('click', function(){
-    if(preview1.style.visibility == "hidden"){
-        preview1.style.visibility = "visible";
+buttonPreview1.addEventListener('click', function(){
+    if(preview1.style.display == "none"){
+        preview1.style.display = "initial";
     }else{
-        preview1.style.visibility = "hidden";
+        preview1.style.display = "none";
+    }
+})
+
+buttonPreview2.addEventListener('click', function(){
+    if(preview2.style.display == "none"){
+        preview2.style.display = "initial";
+    }else{
+        preview2.style.display = "none";
     }
 })
